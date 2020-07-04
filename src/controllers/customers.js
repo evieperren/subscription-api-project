a946eb5491040aa69aec89101cd10f071359ac7e
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-07-04 23:09:40 +0100

    setting up first crud statements

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,17 +1,51 @@
+const mongoose = require('mongoose')
 const Router = require('express').Router
 const customerController = new Router()
+const Customer = mongoose.model('customers', require('../schemas/customers'))
 
 customerController.use('/', (req, res, next) => {
     console.log('reached controllers')
+    next()
 })
 customerController.get('/', async (req, res, next) => {
-    // console.log(res)
     try {
-        res.send('reached customers controller')
+        console.log('create a mentor')
         
     } catch (error) {
         console.log(error)
     }
 })
 
+customerController.post('/', (req, res, next) => {
+    const customer = new Customer({
+        name: {
+            first: 'evie',
+            last: 'perren'
+        },
+        contactDetails: {
+            telephone: "0123456789",
+            email: 'email@email.com',
+            postcode: 'SP10 3XD'
+        },
+        bankDetails: {
+            nameOnAccount: 'EVIE PERREN',
+            accountNumber: 12345678,
+            sortCode: 123456
+        },
+        startDate: '01/09/2020',
+        subscription: {
+            name: 'Small pet plan',
+            level: 'standard',
+            id: "1",
+            activeStatus: 'active'
+        }
+    })
+
+    try {
+        res.send(customer)
+        res.save(customer)
+    } catch (error) {
+        console.log(error)
+    }
+})
 module.exports = customerController
---
+++ src/schemas/customers.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+
+const customerSchema = mongoose.Schema({
+    name: {
+        first: {
+            type: String,
+            required: true
+        },
+        last: {
+            type: String,
+            required: true
+        }
+    },
+    contactDetails: {
+        telephone: {
+            type: String,
+            required: true
+        }, 
+        email: {
+            type: String,
+            required: true
+        },
+        postcode: {
+            type: String,
+            required: true
+        }
+    },
+    bankDetails: {
+        nameOnAccount: {
+            type: String,
+            required: true
+        }, 
+        accountNumber: {
+            type: Number,
+            required: true
+        },
+        sortCode: {
+            type: Number,
+            required: true 
+        }
+    },
+    startDate: {
+        type: String,
+        required: true
+    },
+    subscription: {
+        name: {
+            type: String,
+            required: true
+        },
+        level: {
+            type: String,
+            required: true,
+            enum: ['standard', 'premium', 'deluxe']
+        },
+        id: {
+            type: String,
+            required: true
+        }, 
+        activeStatus: {
+            type: String,
+            required: true,
+            enum: ['active', 'cancelled']
+        }
+    }
+})
+
+module.export = customerSchema

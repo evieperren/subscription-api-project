d3692d4fcf0ca5be50dcf8475272f81f91da97d5
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-07-05 10:36:36 +0100

    playing with POST

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -7,6 +7,7 @@ customerController.use('/', (req, res, next) => {
     console.log('reached controllers')
     next()
 })
+
 customerController.get('/', async (req, res, next) => {
     try {
         console.log('create a mentor')
@@ -16,34 +17,35 @@ customerController.get('/', async (req, res, next) => {
     }
 })
 
-customerController.post('/', (req, res, next) => {
-    const customer = new Customer({
-        name: {
-            first: 'evie',
-            last: 'perren'
-        },
-        contactDetails: {
-            telephone: "0123456789",
-            email: 'email@email.com',
-            postcode: 'SP10 3XD'
-        },
-        bankDetails: {
-            nameOnAccount: 'EVIE PERREN',
-            accountNumber: 12345678,
-            sortCode: 123456
-        },
-        startDate: '01/09/2020',
-        subscription: {
-            name: 'Small pet plan',
-            level: 'standard',
-            id: "1",
-            activeStatus: 'active'
-        }
-    })
-
+customerController.post('/', async (req, res, next) => {
+    
     try {
+        const customer = new Customer(req.body)
+            // name: {
+            //     first: req.body.name.first,
+            //     last: req.body.name.last
+            // },
+            // contactDetails: {
+            //     telephone: "0123456789",
+            //     email: 'email@email.com',
+            //     postcode: 'SP10 3XD'
+            // },
+            // bankDetails: {
+            //     nameOnAccount: 'EVIE PERREN',
+            //     accountNumber: 12345678,
+            //     sortCode: 123456
+            // },
+            // startDate: '01/09/2020',
+            // subscription: {
+            //     name: 'Small pet plan',
+            //     level: 'standard',
+            //     id: "1",
+            //     activeStatus: 'active'
+            // }
+        // })
+    //    await customer.save()
+        console.log(req.body)
         res.send(customer)
-        res.save(customer)
     } catch (error) {
         console.log(error)
     }
